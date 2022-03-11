d8431b3131b898a9e2dcc638b34f00d6fc72f2b1
prbizarro <prbizarro@gmail.com>
2022-03-11 11:46:49 -0300

    interface user repository

---
+++ src/main/java/br/com/pablo/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.pablo.api.repositories;
+
+import br.com.pablo.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}

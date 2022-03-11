b65d91e096f51e6f44eb8f4772c46dcacbf5ab1d
prbizarro <prbizarro@gmail.com>
2022-03-11 15:27:32 -0300

    endpoint create para user

--- src/main/java/br/com/pablo/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/pablo/api/services/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package br.com.pablo.api.services.impl;
 
 import br.com.pablo.api.domain.User;
+import br.com.pablo.api.domain.dto.UserDTO;
 import br.com.pablo.api.repositories.UserRepository;
 import br.com.pablo.api.services.UserService;
 import br.com.pablo.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +19,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
@@ -25,5 +31,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return userRepository.save(mapper.map(obj, User.class));
     }
 }

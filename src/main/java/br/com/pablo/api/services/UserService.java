cd86300b0c6b036c07ea2dbce897158a762b6417
prbizarro <prbizarro@gmail.com>
2022-03-11 14:54:35 -0300

    endpoint findAll para user

--- src/main/java/br/com/pablo/api/services/UserService.java
+++ src/main/java/br/com/pablo/api/services/UserService.java
@@ -2,7 +2,11 @@ package br.com.pablo.api.services;
 
 import br.com.pablo.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+
+    List<User> findAll();
 }
